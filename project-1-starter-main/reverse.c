50ee00c91f10154038c07eb6c7616742a9a4da0f
mcdermott111 <90223154+mcdermott111@users.noreply.github.com>
2022-09-07 16:44:22 -0600

    Project Completed

--- project-1-starter-main/reverse.c
+++ project-1-starter-main/reverse.c
@@ -1,17 +1,20 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
-struct Reverse {
-	char reverse;
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+struct Line {
+   char  *line;
+   struct Line* back;
 };
 
 int main(int argc, char *argv[])
 {
-  	// char *line = NULL;
-  	// size_t len = 0;
-  	// size_t lineSize = 0;
-  	// lineSize = getline(&line, &len, stdin);
-  	
+
 	if (argc == 3) {
+		
 		if (!strcmp(argv[1], argv[2])) {
 			fprintf(stderr, "error: input and output file must differ\n");
 			return 1;
@@ -22,30 +25,37 @@ int main(int argc, char *argv[])
 			fprintf(stderr, "error: cannot open file '%s'\n", argv[1]);
 			return 1;
 		}
-		char ch;
-		ch = fgetc(inputFile);
-		int count = 0;
-		for (char i = getc(inputFile); i != EOF; i = getc(inputFile)) {
-			if (i == '\n'){
-				count = count + 1;
-			}
-		}
-		printf("%d", count);
-		rewind(inputFile);
-		char outputFile[count];
+		struct Line *current = NULL;
+		char *line = (char*) malloc(12*sizeof(char));
 		int newCount = 0;
-		ch = fgetc(inputFile);
-		while (ch != EOF) {
-			ch = fgetc(inputFile);
-			outputFile[newCount] = ch;
+		while (fgets(line, sizeof(line), inputFile)) {
+			
+			struct Line* lineS = (struct Line*) malloc(sizeof(struct Line));
+			lineS->line = line;
+			lineS->back = current;
+			current = lineS;
 			newCount = newCount + 1;
+			line = (char*) malloc(12*sizeof(char));
 		} 
-		printf("%d", newCount);
-		for (int j = newCount - 1; j >= 0; j--) {
-			printf("%c", outputFile[j]);
-		}
 		fclose(inputFile);
-		struct name asdtg
+		FILE* outputFile;
+		outputFile = fopen(argv[2], "w");
+		if (outputFile == NULL) {
+			fprintf(stderr, "error: cannot open file '%s'\n", argv[2]);
+			return 1;
+		}
+
+		while (current != NULL){
+			struct Line* back = current->back;
+			fprintf(outputFile, "%s", current->line);
+			free(current->line);
+			free(current);
+			current = back;
+			
+		}
+		free(line);
+		fclose(outputFile);
+		
 	}
 	else if (argc > 3) {
 		fprintf(stderr, "usage: reverse <input> <output>\n");
@@ -58,13 +68,29 @@ int main(int argc, char *argv[])
 			fprintf(stderr, "error: cannot open file '%s'\n", argv[1]);
 			return 1;
 		}
-		char ch;
-		ch = fgetc(inputFile);
-		printf("%c", ch);
-		while (ch != EOF) {
-			ch = fgetc(inputFile);
-			printf("%c", ch);
+		struct Line *current = NULL;
+		char *line = (char*) malloc(12*sizeof(char));
+		int newCount = 0;
+		while (fgets(line, sizeof(line), inputFile)) {
+			
+			struct Line* lineS = (struct Line*) malloc(sizeof(struct Line));
+			lineS->line = line;
+			lineS->back = current;
+			current = lineS;
+			newCount = newCount + 1;
+			line = (char*) malloc(12*sizeof(char));
 		} 
+		
+		
+		while (current != NULL){
+			struct Line* back = current->back;
+			printf(" %s", current->line);
+			free(current->line);
+			free(current);
+			current = back;
+			
+		}
+		free(line);
 		fclose(inputFile);
 	}
 	
